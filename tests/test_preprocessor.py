7792c815ead45ffb0ef6c40dfbcae974c819a27d
aeg <aeg@amazon.com>
2020-10-02 14:32:25 +1000

    Clean up tests

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -16,10 +16,10 @@ class TestPreprocessor(TestCase):
         tokensier = MagicMock()
         tokensier.tokenize.side_effect = lambda x: x.split(" ")
 
-        label_mapper=MagicMock()
-        label_mapper.entity_labels=["s"]
-        label_mapper.continuation_symbol={"s": "sc"}
-        label_mapper.other_label="o"
+        label_mapper = MagicMock()
+        label_mapper.entity_labels = ["s"]
+        label_mapper.continuation_symbol = {"s": "sc"}
+        label_mapper.other_label = "o"
 
         sut = Preprocessor(max_feature_len=5, tokeniser=tokensier, label_mapper=label_mapper)
         sut._x = x
@@ -34,19 +34,19 @@ class TestPreprocessor(TestCase):
 
     def test__tokenise_label_len_matches_tokens(self):
         # Arrange
-        x = ["Comparison with", "alkaline phosphatases", "and"," "," ", "5-nucleotidase"]
-        y = ["o", "s", "o","o","o", "s"]
+        x = ["Comparison with", "alkaline phosphatases", "and", " ", " ", "5-nucleotidase"]
+        y = ["o", "s", "o", "o", "o", "s"]
 
         # expected_x = ["Comparison", "with", "alkaline", "phosphatases", "and", "5-nucleotidase"]
         # expected_y = ["o", "o", "s", "sc", "o", "s"]
 
         tokensier = MagicMock()
-        tokensier.tokenize.side_effect = lambda z: list(filter(lambda y:y!="",z.split(" ")))
+        tokensier.tokenize.side_effect = lambda z: list(filter(lambda y: y != "", z.split(" ")))
 
-        label_mapper=MagicMock()
-        label_mapper.entity_labels=["s"]
-        label_mapper.continuation_symbol={"s": "sc"}
-        label_mapper.other_label="o"
+        label_mapper = MagicMock()
+        label_mapper.entity_labels = ["s"]
+        label_mapper.continuation_symbol = {"s": "sc"}
+        label_mapper.other_label = "o"
 
         sut = Preprocessor(max_feature_len=5, tokeniser=tokensier, label_mapper=label_mapper)
         sut._x = x
@@ -54,8 +54,6 @@ class TestPreprocessor(TestCase):
 
         # Act
         sut._tokenise()
-        print(sut._x)
-        print(sut._y)
 
         # Assert
         self.assertEqual(len(sut._x), len(sut._y))
