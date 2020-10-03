8fc906b5b0559671cfbe21a963133f6cd39f8f70
aeg <aeg@amazon.com>
2020-10-03 13:24:48 +1000

    Add bert model local testing without internet

--- src/model/bert_model.py
+++ src/model/bert_model.py
@@ -4,10 +4,13 @@ from transformers import BertForTokenClassification
 
 class BertModel(nn.Module):
 
-    def __init__(self, model_name_or_dir, num_classes, fine_tune=True, state_dict=None):
+    def __init__(self, model_name_or_dir, num_classes, fine_tune=True, state_dict=None, config=None):
         super().__init__()
+        if config is None:
+            self.model = BertForTokenClassification.from_pretrained(model_name_or_dir, num_labels=num_classes, state_dict=state_dict)
+        else:
+            self.model = BertForTokenClassification(config)
 
-        self.model = BertForTokenClassification.from_pretrained(model_name_or_dir, num_labels=num_classes, state_dict=state_dict)
         # Fine tune, freeze all other weights except classifier
         if fine_tune:
             self._freeze_base_weights()
---
+++ tests/tests_model/tests_bert_model.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+import torch
+
+import transformers
+
+from model.bert_model import BertModel
+
+
+class TestBertModel(TestCase):
+
+    def test_forward(self):
+        # Bert Config
+        vocab_size = 10
+        sequence_len = 20
+        batch = 32
+        num_classes = 3
+
+        expected_shape = (batch, sequence_len, num_classes)
+
+        input_batch = torch.randint(low=0, high=vocab_size-1, size=(batch, sequence_len))
+        config= transformers.BertConfig(vocab_size=vocab_size,hidden_size=10, num_hidden_layers=1, num_attention_heads=1,num_labels=num_classes)
+        sut = BertModel(None, None, config=config)
+
+        # Act
+        actual = sut.forward(input_batch)[0]
+
+        # Assert
+        self.assertEqual(expected_shape, actual.shape)
