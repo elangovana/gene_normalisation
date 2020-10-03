e47204e11f0635bf375522052a7887714e1beaf4
aeg <aeg@amazon.com>
2020-10-03 17:25:51 +1000

    Fix top k loss

--- src/top_k_cross_entropy_loss.py
+++ src/top_k_cross_entropy_loss.py
@@ -23,7 +23,7 @@ class TopKCrossEntropyLoss(nn.Module):
         # remove all entries where the target is -1 (pseudo index), indicating predictions for PAD character that we dont really care about
         mask = (target != self.pad_index_label).long().to(device=predicted.device)
 
-        p_mask =  torch.unsqueeze(mask, dim=1)
+        p_mask = torch.unsqueeze(mask, dim=1)
 
         # zero out the values for pad index in target and predicted, so that they don't count
         predicted_zerod = predicted * p_mask
@@ -32,10 +32,14 @@ class TopKCrossEntropyLoss(nn.Module):
         # make sure k is within the length of the target shape
         k = min(self.k, target_zerod.shape[0])
 
-        loss_per_item = self._loss_func(predicted_zerod, target_zerod)
+        loss_per_item = self._loss_func(predicted_zerod, target_zerod) * mask
+
+        # Only compute loss where mask is non zero
+        non_zero_average_loss_per_item = loss_per_item.sum(dim=1) / mask.sum(dim=1)
 
         # Obtain only the top k hard samples
-        top_k_loss = torch.mean(torch.topk(loss_per_item, k=k)[0])
-        self._logger.debug("Total loss {} vs topk loss {}".format(torch.mean(loss_per_item), top_k_loss))
+        top_k_loss = torch.mean(torch.topk(non_zero_average_loss_per_item, k=k)[0])
+        self._logger.debug(
+            "Total loss {} vs topk loss {}".format(torch.mean(non_zero_average_loss_per_item), top_k_loss))
 
         return top_k_loss
